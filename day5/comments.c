cd9d147f8aeaa6bc8a63a4289f3a09c66521070a
Gatte Pushpasri <pushpasri@gmail.com>
2025-07-29 21:25:23 +0530

    day4: Add comments and readability example
    - Added new file: demonstrating use of single-line and multi-line comments
    - Improved readability in the code

---
+++ day5/comments.c
@@ -0,0 +1,61 @@
+/*   Code & Readability:
+
+Code readability means how easy it is for someone else (or future you!) to read, understand, and follow your code.
+
+Readable code:
+
+Explains what it does clearly
+
+Is clean, consistent, and well-organized
+
+Is easier to debug, maintain, and extend
+
+Why readability matters:
+
+Makes teamwork easier (others can quickly understand your code)
+
+Helps you later (you’ll understand your own code months later!)
+
+Reduces bugs
+
+Makes it easier to add new features or fix problems
+
+Tips to write readable code (use today!)
+
+*/
+
+// 1. Use meaningful names:
+// Bad
+int a, b;
+
+// Good
+int studentCount, totalMarks;
+
+/* comments:
+
+Comments are notes you write in your code that the compiler completely ignores.
+
+They help:
+
+Explain what your code does
+
+Make code easier to read and understand
+
+Remind yourself or others why you wrote something in a certain way
+
+Types of comments in C
+
+1️.Single-line comment
+Starts with //
+// This is a single-line comment
+printf("Hello, World!\n"); // print a message
+Use these for short explanations or at the end of a line.
+
+2️.Multi-line (block) comment
+
+Starts with /* and ends with */
+/*
+ This is a multi-line comment.
+ You can write explanations here.
+ Useful for larger notes.
+*/
